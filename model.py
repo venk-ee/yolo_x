34e159603ef558329ea547f2a24fd141ac00ce1c
venkatanatha <venkatanathaav@gmail.com>
2026-05-28 09:49:12 +0530

    refactor: update CSPDarknet backbone architecture and clean up data loading logic

--- model.py
+++ model.py
@@ -67,37 +67,24 @@ class YOLOXNeck(nn.Module):
         )
         # PAN Neck Downsample
         self.downsample_1 = nn.Conv2d(
-            in_channels=256,
-            out_channels=256,
-            kernel_size=3,
-            stride=2,
-            padding=1
-            )
-
-        self.downsample_block_1=CSPBlock(
-            in_channels=512,
-            out_channels=256,
-            num_blocks=5,
-            shortcut=False
+            in_channels=256, out_channels=256, kernel_size=3, stride=2, padding=1
         )
 
-        self.downsample_2=nn.Conv2d(in_channels=256,
-            out_channels=256,
-            kernel_size=3,
-            stride=2,
-            padding=1
-            )
-        self.down_block_2 = CSPBlock(in_channels=512,
-         out_channels=256,
-          num_blocks=5,
-           shortcut=False
-           )
-        
+        self.downsample_block_1 = CSPBlock(
+            in_channels=512, out_channels=256, num_blocks=5, shortcut=False
+        )
 
-
+        self.downsample_2 = nn.Conv2d(
+            in_channels=256, out_channels=256, kernel_size=3, stride=2, padding=1
+        )
+        self.down_block_2 = CSPBlock(
+            in_channels=512, out_channels=256, num_blocks=5, shortcut=False
+        )
 
     def forward(self, backbone_outputs):
-        shallow, mid, deep = backbone_outputs #[1, 256, 80, 80],[1, 256, 40, 40],[1, 256, 20, 20]
+        shallow, mid, deep = (
+            backbone_outputs  # [1, 256, 80, 80],[1, 256, 40, 40],[1, 256, 20, 20]
+        )
 
         deep_upsampled = F.interpolate(deep, scale_factor=2, mode="nearest")
 
@@ -105,17 +92,19 @@ class YOLOXNeck(nn.Module):
 
         mid_processed = self.up_upsample_block_1(fused_mid)
 
-
-        mid_processed_upwnsampled=F.interpolate(mid_processed,scale_factor=2,mode="nearest")
+        mid_processed_upwnsampled = F.interpolate(
+            mid_processed, scale_factor=2, mode="nearest"
+        )
 
         fused_shallow = torch.cat((mid_processed_upwnsampled, shallow), dim=1)
-
 
         shallow_processed = self.up_upsample_block_2(fused_shallow)
 
         shallow_downsamplled = self.downsample_1(shallow_processed)
 
-        shallow_downsamplled_concated = torch.cat((shallow_downsamplled, mid_processed), dim=1)
+        shallow_downsamplled_concated = torch.cat(
+            (shallow_downsamplled, mid_processed), dim=1
+        )
 
         mid_pan_out = self.downsample_block_1(shallow_downsamplled_concated)
 
@@ -125,12 +114,11 @@ class YOLOXNeck(nn.Module):
 
         deep_processed = self.down_block_2(mid_downsamplled_concated)
 
-
         return shallow_processed, mid_pan_out, deep_processed
 
 
 class DecopledHead(nn.Module):
-    def __init__(self,in_channels:int,out_channels:int,num_classes:int):
+    def __init__(self, in_channels: int, out_channels: int, num_classes: int):
         super().__init__()
 
         self.cls_convs = nn.Sequential(
@@ -139,7 +127,7 @@ class DecopledHead(nn.Module):
             nn.SiLU(),
         )
 
-        self.reg_convs=nn.Sequential(
+        self.reg_convs = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
             nn.BatchNorm2d(out_channels),
             nn.SiLU(),
@@ -149,18 +137,15 @@ class DecopledHead(nn.Module):
         self.reg_pred = nn.Conv2d(out_channels, 4, kernel_size=3, padding=1)
         self.obj_pred = nn.Conv2d(out_channels, 1, kernel_size=3, padding=1)
 
+    def forward(self, x):
+        cls_feat = self.cls_convs(x)
+        reg_feat = self.reg_convs(x)
 
-    
+        cls_out = self.cls_pred(cls_feat)
+        reg_out = self.reg_pred(reg_feat)
+        obj_out = self.obj_pred(reg_feat)
 
-    def forward(self,x):
-        cls_feat=self.cls_convs(x)
-        reg_feat=self.reg_convs(x)
-
-        cls_out=self.cls_pred(cls_feat)
-        reg_out=self.reg_pred(reg_feat)
-        obj_out=self.obj_pred(reg_feat)
-
-        return cls_out,reg_out,obj_out
+        return cls_out, reg_out, obj_out
 
 
 class CSPDarknet_Backbone(nn.Module):
@@ -170,47 +155,66 @@ class CSPDarknet_Backbone(nn.Module):
 
         # Stage 1: 64 → 128
         self.down1 = nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1)
-        self.csp1  = CSPBlock(128, 128, num_blocks=3)
+        self.csp1 = CSPBlock(128, 128, num_blocks=3)
 
         # Stage 2: 128 → 256  ← shallow output
         self.down2 = nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1)
-        self.csp2  = CSPBlock(256, 256, num_blocks=3)
+        self.csp2 = CSPBlock(256, 256, num_blocks=3)
 
         # Stage 3: stays 256  ← mid output
-        self.down3 = nn.Conv2d(256, 256, 3, stride=1, padding=1)
-        self.csp3  = CSPBlock(256, 256, num_blocks=3)
+        self.down3 = nn.Conv2d(256, 256, 3, stride=2, padding=1)
+        self.csp3 = CSPBlock(256, 256, num_blocks=3)
 
         # Stage 4: stays 256  ← deep output
-        self.down4 = nn.Conv2d(256, 256, 3, stride=1, padding=1)
-        self.csp4  = CSPBlock(256, 256, num_blocks=3)
+        self.down4 = nn.Conv2d(256, 256, 3, stride=2, padding=1)
+        self.csp4 = CSPBlock(256, 256, num_blocks=3)
 
-    def forward(self,x):
-        x=self.conv1(x)
-        x=self.csp1_conv(x)
-        x=self.csp1(x)
-        x=self.csp2(x)
-        x=self.csp3(x)
-        return x
+    def forward(self, x):
+            # Image comes in at 640x640
+            x = self.stem(x) # Drops to 320x320
+            
+            # Stage 1: We don't need this feature map, it's too big (160x160). 
+            # So we just keep calling it 'x' and pass it along.
+            x = self.down1(x)
+            x = self.csp1(x)
+            
+            # Stage 2: The Intern! (80x80). We want to save this!
+            x = self.down2(x)
+            shallow = self.csp2(x)
+            
+            # Stage 3: The Manager! (40x40). We want to save this!
+            x = self.down3(shallow)
+            mid = self.csp3(x)
+            
+            # Stage 4: The CEO! (20x20). We want to save this!
+            x = self.down4(mid)
+            deep = self.csp4(x)
+            
+            return shallow, mid, deep
 
 class YOLOX(nn.Module):
-    def __init__(self,num_classes:int):
+    def __init__(self, num_classes: int):
         super().__init__()
 
-        self.backbone=CSPDarknet_Backbone()
-        self.neck=YOLOXNeck()
+        self.backbone = CSPDarknet_Backbone()
+        self.neck = YOLOXNeck()
 
-        self.head_0 =DecopledHead(in_channels=256,out_channels=256,num_classes=num_classes)
-        self.head_1 =DecopledHead(in_channels=256,out_channels=256,num_classes=num_classes)
-        self.head_2 =DecopledHead(in_channels=256,out_channels=256,num_classes=num_classes)
-    
+        self.head_0 = DecopledHead(
+            in_channels=256, out_channels=256, num_classes=num_classes
+        )
+        self.head_1 = DecopledHead(
+            in_channels=256, out_channels=256, num_classes=num_classes
+        )
+        self.head_2 = DecopledHead(
+            in_channels=256, out_channels=256, num_classes=num_classes
+        )
 
-    def forward(self,x):
-        x=self.backbone(x)
-        outputs=self.neck(x)
+    def forward(self, x):
+        x = self.backbone(x)
+        outputs = self.neck(x)
 
         shallow_out = self.head_0(outputs[0])
-        mid_out= self.head_1(outputs[1])
+        mid_out = self.head_1(outputs[1])
         deep_out = self.head_2(outputs[2])
 
-        return shallow_out,mid_out,deep_out
-        
+        return shallow_out, mid_out, deep_out
